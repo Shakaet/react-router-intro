23ee5c83233aff9ad17925f36c4abcfcf4c8adf9
Shakaet <abdshakaet@gmail.com>
2024-10-29 02:30:43 +0600

    add

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 import Footer from '../footer/Footer';
 
 const Header = () => {
+    const navigation = useNavigation();
+    let location= useLocation()
+    console.log(location)
     return (
         <div>
 
             <Nav></Nav>
+
+            {
+                 navigation.state === "loading"
+                ? <p>loading...</p>
+                : <Outlet></Outlet>
+            }
     
-            <Outlet></Outlet>
+            {/* <Outlet></Outlet> */}
 
             <Footer></Footer>
             
